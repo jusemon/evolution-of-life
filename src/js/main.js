f1072006e192942ed50f2814a65a0d6413f72353
Juan Sebastian Montoya <juansmm@outlook.com>
2023-04-21 12:16:41 -0500

    Added proper canvas size managment

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,5 @@
 import { init, Sprite, SpriteSheet, GameLoop, initKeys, keyPressed } from "kontra";
-
-// Define default values and helpers
-const defaultWidth = 160;
-const defaultHeight = 120;
-const padding = 20;
-const { innerWidth } = window;
-const sizeFactor = defaultWidth > innerWidth ? 1 : (innerWidth - padding) / defaultWidth;
-
-const scl = (value) => value * sizeFactor;
+import CanvasWindow from "./classes/canvas-windows";
 
 /**
  * Draws a pixel art sprite from a 2D array of pixel data.
@@ -19,18 +11,16 @@ const drawPixels = (spriteData) => {
         try {
             const ctx = document.createElement('canvas').getContext("2d");
             ctx.fillStyle = "black";
-            ctx.strokeStyle = "black";
             ctx.beginPath();
             for (let yIndex = 0; yIndex < spriteData.length; yIndex++) {
                 const row = spriteData[yIndex];
                 for (let xIndex = 0; xIndex < row.length; xIndex++) {
                     const pixel = row[xIndex];
                     if (pixel) {
-                        ctx.fillRect(scl(xIndex), scl(yIndex), scl(1), scl(1));
+                        ctx.fillRect(xIndex, yIndex, 1, 1);
                     }
                 }
             }
-            ctx.fill();
             const img = document.createElement('img');
             img.onload = function () {
                 resolve(img)
@@ -46,21 +36,35 @@ const drawPixels = (spriteData) => {
 }
 
 (async () => {
+    const { canvas } = init();
+    const canvasWindow = new CanvasWindow({
+        nativeWidth: 160,
+        nativeHeight: 120,
+        maxMultiplier: 10,
+        windowPercentage: .9,
+        canvas
+    })
+    window.addEventListener('resize', () => {
+        canvasWindow.resize();
+    })
+    
+    window.addEventListener('load', () => {
+        // initialize native height/width
+        canvas.width = canvasWindow.canvasWidth;
+        canvas.height = canvasWindow.canvasHeight;
+        canvasWindow.resize();
+    })
+    
     // Create and scale canvas based on screen size
-    const canvasEl = document.createElement('canvas');
-    canvasEl.width = scl(defaultWidth);
-    canvasEl.height = scl(defaultHeight);
-    document.body.appendChild(canvasEl);
-    let { canvas } = init();
     const spriteData = [
         [, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , ,],
-        [, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , ,],
-        [, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , ,],
-        [, , , , , , , , , ,1,1,1,1, , , , , , , , , , , , , , , , , ,],
-        [, ,1,1,1,1, , , , ,1, , ,1, , , , , ,1,1,1, , , , ,1,1,1, , ,],
-        [, ,1, , ,1, , , , ,1, , ,1, , , , , ,1, ,1, , , , ,1, ,1, , ,],
-        [, ,1, , ,1, , , , ,1, , ,1, , , , ,1,1, ,1, , , , ,1, ,1,1, ,],
-        [, ,1,1,1,1, , , , ,1,1,1,1, , , , ,1,1,1,1, , , , ,1,1,1,1, ,],
+        [, , , , , , , , ,1,1,1,1,1,1, , , , , , , , , , , , , , , , ,],
+        [,1,1,1,1,1,1, , ,1,1,1,1,1,1, , , ,1,1,1,1,1, , ,1,1,1,1, , ,],
+        [,1,1,1,1,1,1, , ,1,1, , ,1,1, , , ,1,1,1,1,1, , ,1,1,1,1, , ,],
+        [,1,1, , ,1,1, , ,1,1, , ,1,1, , , ,1,1, , ,1, , ,1, , ,1, , ,],
+        [,1,1, , ,1,1, , ,1,1, , ,1,1, , ,1,1,1, , ,1, , ,1, , ,1,1, ,],
+        [,1,1,1,1,1,1, , ,1,1,1,1,1,1, , ,1,1,1,1,1,1, , ,1,1,1,1,1, ,],
+        [,1,1,1,1,1,1, , ,1,1,1,1,1,1, , ,1,1,1,1,1,1, , ,1,1,1,1,1, ,],
     ];
     const anims = {
         walkright: 'walkright',
@@ -69,8 +73,8 @@ const drawPixels = (spriteData) => {
     }
     const spriteSheet = SpriteSheet({
         image: await drawPixels(spriteData),
-        frameWidth: scl(8),
-        frameHeight: scl(8),
+        frameWidth: 8,
+        frameHeight: 8,
         animations: {
             [anims.walkright]: {
                 frames: [0, 2],
@@ -82,14 +86,14 @@ const drawPixels = (spriteData) => {
             },
             [anims.idle]: {
                 frames: [0, 1],
-                frameRate: 5
+                frameRate: 5,
             }
         }
     });
 
     const sprite = Sprite({
         x: 0,
-        y: scl(defaultHeight) - scl(8),
+        y: canvasWindow.nativeHeight - 8,
         animations: spriteSheet.animations
     })
 
@@ -97,27 +101,25 @@ const drawPixels = (spriteData) => {
 
     sprite.playAnimation(anims.idle);
 
-    const loop = GameLoop({  // create the main game loop
-        update: function () { // update the game state
+    const loop = GameLoop({ 
+        update: function () {
             sprite.update();
             if (keyPressed('arrowright')) {
                 sprite.playAnimation(anims.walkright)
-                sprite.x += 2;
+                sprite.x += 1;
             } else if (keyPressed('arrowleft')) {
                 sprite.playAnimation(anims.walkleft)
-                sprite.x -= 2;
+                sprite.x -= 1;
             } else {
                 sprite.playAnimation(anims.idle)
             }
-            // wrap the sprites position when it reaches
-            // the edge of the screen
             if (sprite.x > canvas.width) {
                 sprite.x = -sprite.width;
             } else if (sprite.x < -sprite.width) {
                 sprite.x = canvas.width;
             }
         },
-        render: function () { // render the game state
+        render: function () {
             sprite.render();
         }
     });
